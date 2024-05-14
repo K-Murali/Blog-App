6fc5c6ccd0949c91a9b0ada533bde938a5d38e81
K-Murali <kmuralidhar888@gmail.com>
2024-05-14 18:38:18 +0530

    port changed

--- backend/index.js
+++ backend/index.js
@@ -4,14 +4,14 @@ const connectToMongo=require('./db');
 connectToMongo();
 
 const app = express();
-const port = 5000
+const PORT = process.env.PORT||5000
 app.use(cors());
 app.use(express.json()); 
 
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'))
 
-app.listen(port, () => {
-  console.log(`Example app listening on port https://localhost:${port}`)
+app.listen(PORT, () => {
+  console.log(`Example app listening on port https://localhost:${PORT}`)
 
 })
