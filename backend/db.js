73c242c572cbc5efcdbd5bda7dd8ec45ab20c2bd
K-Murali <kmuralidhar888@gmail.com>
2024-05-14 18:07:24 +0530

    db updated

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,17 @@
-const mongoose = require('mongoose');
-const mongoURI='mongodb://127.0.0.1:27017/inotebook';
+const mongoose = require("mongoose");
 
-const  connectToMongo=()=>{
-    mongoose.connect(mongoURI)
-}
-module.exports=connectToMongo;
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+    "<password>",
+    process.env.DATABASE_PASSWORD
+);
+// console.log(process.env.USERNAME);
+
+const connectToMongo = () => {
+  mongoose.connect(DB, {}).then((con) => {
+    console.log("DB connection successful");
+  });
+};
+module.exports = connectToMongo;
