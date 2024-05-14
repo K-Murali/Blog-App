ad31276de9c3cc2912cd6d096069412cce1f0897
K-Murali <kmuralidhar888@gmail.com>
2024-05-14 19:07:33 +0530

    latest

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,16 @@
-const express = require('express')
-var cors=require('cors')
-const connectToMongo=require('./db');
+const express = require("express");
+const cors = require("cors");
+const connectToMongo = require("./db");
 connectToMongo();
 
 const app = express();
-const PORT = process.env.PORT||5000
+const PORT = process.env.PORT || 5000;
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-app.use('/api/auth',require('./routes/auth'));
-app.use('/api/notes',require('./routes/notes'))
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port https://localhost:${PORT}`)
-
-})
+  console.log(`Example app listening on port https://localhost:${PORT}`);
+});
